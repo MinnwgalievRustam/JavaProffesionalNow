1b978037949abc17a0776f071cc1e03395731b96
Rustam <rustamminnegaliev1997@gmail.com>
2022-01-15 20:53:03 +0500

    new commit

---
+++ src/main/java/regular_task/Customer.java
@@ -0,0 +1,11 @@
+package regular_task;
+
+public class Customer {
+    String login;
+    String password;
+
+    public Customer(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
